231abadf6b1c531c200ed9f9abf1efbfe86496bb
Votre Nom <odilecoutiez@gmail.com>
2019-10-28 16:05:40 +0100

    Exercice-2 Js Part-3

---
+++ fichier.js
@@ -0,0 +1,8 @@
+//on declare une fonction qui va faire disparaitre les element dans le Id "text"
+function hideText(){
+  document.getElementById('text').style.display = 'none';
+}
+//on declare une fonction qui va faire réaparaitre les element dans le Id "text"
+function showText(){
+  document.getElementById('text').style.display = 'block';
+}
